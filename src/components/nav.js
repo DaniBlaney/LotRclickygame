131fb6cf03abb0d6b673a45042d032f86e62f717
DaniBlaney <daniblaney@gmail.com>
2019-07-06 18:31:49 -0400

    fragments

--- src/components/nav.js
+++ src/components/nav.js
@@ -5,11 +5,13 @@ const NavBar = props => {
   return (
     <div className="NavBar row">
      {
-       
+      <>
+        <div className="score col-md">Score: {props.score}</div>
+        <div className="score col-md">Message: {props.message}</div>
+        <div className="score col-md">Top Score: {props.topScore}</div>
+      </>
      }
-      <div className="score col-md">Score: {props.score}</div>
-      <div className="score col-md">Message: {props.message}</div>
-      <div className="score col-md">Top Score: {props.topScore}</div>
+
     </div>
   );
 };
