3c71abea460045ee627f9cc522b908fc0cc3cd33
DaniBlaney <daniblaney@gmail.com>
2019-07-06 16:09:38 -0400

    footer

---
+++ src/components/footer.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './style.css';
+
+const Footer = () => <div className="footer text-center">© 2019 - Site Built By Ms. Danielle Blaney</div>;
+
+export default Footer;
