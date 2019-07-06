d281f5c002291ab906f7f2e19b28b40b89803db9
DaniBlaney <daniblaney@gmail.com>
2019-07-06 17:27:37 -0400

    directions

--- src/components/instructions.js
+++ src/components/instructions.js
@@ -5,7 +5,7 @@ const Instructions = () => {
   return (
     <div className="instructions">
       <h1 className="instructions">Lord of the Rings Clicky Game!</h1>
-      <h5 className="instructions">
+      <h5 className="instructions info">
         Click on an image to earn points, but don't click on any more than once!
       </h5>
     </div>
