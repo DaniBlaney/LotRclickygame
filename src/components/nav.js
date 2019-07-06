40a557a2dd2be7a5e9ab91d44cb0c5aabc4f9b52
DaniBlaney <daniblaney@gmail.com>
2019-07-06 18:11:11 -0400

    row

--- src/components/nav.js
+++ src/components/nav.js
@@ -4,9 +4,9 @@ import "./style.css";
 const NavBar = props => {
   return (
     <div className="NavBar row">
-      {/* <div className="score">
-      Score {props.score}  Message {props.message} Top Score {props.topScore}
-      </div> */}
+     {
+       
+     }
       <div className="score col-md">Score: {props.score}</div>
       <div className="score col-md">Message: {props.message}</div>
       <div className="score col-md">Top Score: {props.topScore}</div>
